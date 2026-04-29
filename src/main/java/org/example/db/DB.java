f78653b87436d411fbd518dc08588f3c1ac6439a
becaaaaaadevdec <20231ireinfint0003@ifba.edu.br>
2026-04-29 10:04:31 -0300

    Fizemos a conexao!!!!

---
+++ src/main/java/org/example/db/DB.java
@@ -0,0 +1,50 @@
+//metodos acessores do banco do dados
+package org.example.db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+    public static Connection conn = null; // Criar variavel para a conec, armazena conec
+
+
+    public static Connection getConnection(){ // esse comando serve para conectar com o banco de daDOS
+       if(conn == null){// para verificar se estar conectado, null nao esta
+           try {
+
+               Properties props = loadProperties();
+
+               String url = props.getProperty("dburl");
+
+               conn = DriverManager.getConnection(url,props); // parte principal que coneecta com o banco
+                //variavel conn recebe uma conexao
+               // criado 2 variaveis pq? pra nao consumir recurso desnecessario
+
+           } catch (SQLException e) {
+               throw new DbException(e.getMessage());
+           }
+       }
+       return conn; //
+    }
+
+    public static Properties loadProperties() {
+
+        try (FileInputStream fs = new FileInputStream("db.properties")){
+            Properties props = new Properties();
+
+            props.load(fs);
+
+            return props;
+        }catch(IOException e){
+            throw new DbException(e.getMessage());
+        }
+    }
+            }
+// propties e um tipo de dado
+
+// esse codigo serve para apenas entrar/loguin e colocar a senha no banco de dados (MYsql)
